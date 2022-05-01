19d23eac818247a53f400a0b116d169c79ec8849
MarioDhiego <mario.valente@detran.pa.gov.br>
2022-05-01 15:42:31 -0300

    atualização de script

--- MAPA_COVID19_PARA.R
+++ MAPA_COVID19_PARA.R
@@ -12,7 +12,7 @@ rm(list = ls())
 
 ###### INSTALACAO DOS PACOTES #########################################################
 ######
-# Serão Instalados os Diversos Pacotes
+# Ser?o Instalados os Diversos Pacotes
 
 install.packages(c("sf","geobr","magrittr","dplyr","colorspace",
                    "ggplot2","gifski","gganimate","leaflet","maps", "sp"))
@@ -25,6 +25,7 @@ library(sf)         # Ler aquivos tipo .shap
 library(geobr)      # Base Cartográfica/IBGE
 library(magrittr)   # utilizar operador pipe
 library(dplyr)      # fazer manipula??o no banco
+library(readr)
 library(colorspace) # usar paleta de cores
 library(ggplot2)    # gerar mapa por camadas
 library(gifski)     # Highest Quality GIF Encoder 
@@ -32,6 +33,12 @@ library(gganimate)  # gerar animação no gráfico
 library(leaflet)    # gerar mapas interativos
 library(maps)       # Draw Geographical Maps
 library(sp)         # Classes and Methods for Spatial Data 
+library(crul)
+library(htmltools)
+library(covr)
+library(rgdal)
+library(knitr)
+library(rmarkdown)
 #######################################################################################
 
 
@@ -42,22 +49,47 @@ library(sp)         # Classes and Methods for Spatial Data
 # DATA : 27/03/2021
 #######################################################################################
 
+summary(casosPA)
+
 
 ###### Manipulação de Leitura da Base Cartográica ###############################################
 # Ler base cartográfica do IBGE via Pacote geobr
 BASE_PA <- read_municipality(code_muni = "PA", year = 2020)
+BASE_SP <- read_municipality(code_muni = "SP", year = 2020)
+BASE_RJ <- read_municipality(code_muni = "RJ", year = 2020)
 
 # Mapa simples via pacote base
 plot(BASE_PA)
+plot(BASE_SP)
+plot(BASE_RJ)
 
 # Mapa simples via pacote ggplo2
+# Mapa do Pará
 ggplot(BASE_PA)+
   geom_sf(fill= "#2D3E50", color= "#FEBF57", 
           size=0.15, show.legend = FALSE)
+
+
+# Mapa de São Paulo
+ggplot(BASE_SP)+
+  geom_sf(fill= "#2D3E50", color= "#FEBF57", 
+          size=0.15, show.legend = FALSE)
+
+# Mapa dO Rio de Janeiro
+ggplot(BASE_RJ)+
+  geom_sf(fill= "#2D3E50", color= "#FEBF57", 
+          size=0.15, show.legend = FALSE)
+
 ########################################################################################
 
 
 ###### Manipulação da Base de COVID19 ##############################################
+
+
+COVID19_MAR_PA <- read_csv("COVID19_MAR_PA.csv")
+
+
+
 ######
 # Limpar Casos
 casosPA <- COVID19_MAR_PA
@@ -75,18 +107,19 @@ casosPA <- casosPA[,-colunas]
 
 ####### Juntar as Bases (geobr+ COVID19_PA) ##########################################
 #######
-PA_Casos_Covid <- merge(BASE_PA, casosPA, by.x= "code_muni", by.y="city_ibge_code")
+PA_Casos_Covid <- merge(BASE_PA, casosPA, 
+                        by.x= "code_muni", 
+                        by.y="city_ibge_code")
 
 # Gerar o Mapa (gerobr + COVID19_PA)
-Mapa_PA <- leaflet(PA_Casos_Covid) %>% 
-  addTiles()
+Mapa_PA <- leaflet(PA_Casos_Covid) %>% addTiles()
 Mapa_PA %>% addPolygons()
 
 Mapa_PA %>% addPolygons(
   weight = 1.5,
   opacity = 0.5,
   color = "blue",
-  dashArray = 1,
+  dashArray = "1",
   smoothFactor = 1.5,
   fillOpacity = 0,)
 ######################################################################################
@@ -95,13 +128,13 @@ Mapa_PA %>% addPolygons(
 ###### Definir as Categorias da Legenda e Paletas de cores ###########################
 ######
 # Bins e Cores/ pacote (colorspace)
-Categoria_Legenda <- c(0,10,20,50,100,500,Inf)
+Categoria_Legenda <- c(0,10,20, 50, 100,1000, Inf)
 pal <- colorBin("YlOrRd", domain = PA_Casos_Covid$deaths, bins = Categoria_Legenda)
 
 Mapa_PA %>% addPolygons(
   fillColor = ~pal(deaths),
-  weight = 2.0,
-  opacity = 1.0,
+  weight = 2,
+  opacity = 1,
   color = "white",
   dashArray = 1,
   smoothFactor = 1.5,
@@ -113,7 +146,7 @@ Mapa_PA %>% addPolygons(
 ######
 Mapa_PA %>% addPolygons(
   fillColor = ~pal(deaths),
-  weight = 1.5,
+  weight = 2,
   opacity = 1,
   color = "white",
   dashArray = "1",
@@ -128,11 +161,11 @@ Mapa_PA %>% addPolygons(
 ########################################################################################
 
 
-###### Customizando InformaçÕES/gerar html #############################################
+###### Customizando Informa??es/gerar html #############################################
 ######
 label1 <- sprintf(
-  "<strong>%s</strong></br>%g Confirmados</br>%g ?bitos",
-  PA_Casos_Covid$name_muni, PA_Casos_Covid$confirmed, PA_Casos_Covid$deaths) %>% lapply(htmltools::HTML)
+  "<strong>%s</strong></br>%g Acidentes</br>%g Vitima Fatal",
+  PA_Casos_Covid$name_muni, PA_Casos_Covid$confirmed, PA_Casos_Covid$fatal) %>% lapply(htmltools::HTML)
 
 # Mapa Customizado
 
@@ -158,13 +191,13 @@ Mapa_PA %>% addPolygons(
   )) %>% addLegend(pal= pal,
                    values = ~deaths,
                    opacity = 0.5,
-                   title = "Casos de ?bitos - COVID19",
+                   title = "Vitma Fatal no Transito",
                    position= "bottomright")
 
 #################################################################################################
 
 ####### Salvar o Mapa ###########################################################################
-ggsave(plot = meu.plot,filename="C:/Users/mario Dhiego/Documents/Mapas_Covid19/mapa1_covid.jpeg",
+ggsave(plot = meu.plot,filename="C:/Users/mario Dhiego/Documents/Mapas_Covid19_PARA/Mapas.COVID19.PARA/mapa1_vitima.jpeg",
        width= 5,height=5)
 #################################################################################################
 
